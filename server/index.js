25b983e69884ca56d8146537081409d3649eb0f7
ga-possum <aleks.khristof@gmail.com>
2020-02-04 20:05:43 -0500

    general setup

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const ejs = require('ejs');
+const bp = require('body-parser');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(bp.json());
+app.engine('html', ejs.renderFile);
+app.get('/:id', (req, res) => {
+  res.render('../public/index.html');
+});
+app.set('view engine', 'html');
+
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => { console.log(`Listening on port ${port}`); });
